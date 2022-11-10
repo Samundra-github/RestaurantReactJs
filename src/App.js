12b6152cafe41fdacffe389c05a7762f24d3b491
Samundra Bhandari <76542341+Samundra-github@users.noreply.github.com>
2022-11-10 18:31:51 +0545

    React Restaurant app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-// import React from 'react'
+import React from 'react';
+import Restaurant from './component/Basics/Restaurant';
 
 const App = () => {
   return (
-    <>
-      Hello World
-    </>
+    <Restaurant/>
   )
 }
 
