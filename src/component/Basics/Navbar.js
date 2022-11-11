a2c821dda24fa6bd31c8451fbd679a19f555ea93
Samundra Bhandari <76542341+Samundra-github@users.noreply.github.com>
2022-11-11 13:11:52 +0545

    React Restaurant app

--- src/component/Basics/Navbar.js
+++ src/component/Basics/Navbar.js
@@ -1,34 +1,24 @@
-import React from 'react'
 
-const Navbar = ({filterItem, menuList}) => {
-    return (
-        <>
-            <nav className='navbar'>
-                <div className='btn-group'>
-                    <button
-                        className='btn-group__item'
-                        onClick={() => filterItem("breakfast")}>
-                        Breakfast
-                    </button>
-                    <button
-                        className='btn-group__item'
-                        onClick={() => filterItem("lunch")}>
-                        Lunch
-                    </button>
-                    <button
-                        className='btn-group__item'
-                        onClick={() => filterItem("evening")}>
-                        Evening
-                    </button>
-                    <button
-                        className='btn-group__item'
-                        onClick={() => filterItem("dinner")}>
-                        Dinner
-                    </button>
-                </div>
-            </nav>
-        </>
-    )
-}
+import React from "react";
 
-export default Navbar
+const navbar = ({ filterItem, menuList }) => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="btn-group">
+          {menuList.map((curElem) => {
+            return (
+              <button
+                className="btn-group__item"
+                onClick={() => filterItem(curElem)}>
+                {curElem}
+              </button>
+            );
+          })}
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default navbar;
--- src/component/Basics/Restaurant.js
+++ src/component/Basics/Restaurant.js
@@ -1,37 +1,43 @@
-import React, { useState } from 'react'
-import "./style.css"
-import Menu from "./menuApi"
-import MenuCard from './MenuCard'
-import Navbar from './Navbar'
+import React, { useState } from "react";
+import "./style.css";
+import Menu from "./menuApi.js";
+import MenuCard from "./MenuCard";
+import Navbar from "./Navbar";
 
 const uniqueList = [
-    ... new Set(
+    ...new Set(
         Menu.map((curElem) => {
-            return curElem.category
+            return curElem.category;
         })
-    )
-]
-console.log(uniqueList)
+    ),
+    "All",
+];
 
-const Restaurant = () => {
-    const [menuData, setMenuData] = useState(Menu)
+console.log(uniqueList);
 
-    const [menuList, setMenuList] = useState(uniqueList)
+const Resturant = () => {
+    const [menuData, setMenuData] = useState(Menu);
+    const [menuList, setMenuList] = useState(uniqueList);
 
     const filterItem = (category) => {
+        if (category === "All") {
+            setMenuData(Menu);
+            return;
+        }
+
         const updatedList = Menu.filter((curElem) => {
             return curElem.category === category;
         });
-        setMenuData(updatedList);
-    }
 
+        setMenuData(updatedList);
+    };
 
     return (
         <>
-            <Navbar filterItem = { filterItem } menuList={ menuList }/>
+            <Navbar filterItem={filterItem} menuList={menuList} />
             <MenuCard menuData={menuData} />
         </>
-    )
-}
+    );
+};
 
-export default Restaurant
+export default Resturant;
